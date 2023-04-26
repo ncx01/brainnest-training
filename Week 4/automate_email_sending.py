2e4be927994848eeaec3abd83c4e9f226f776136
ncx01 <nicholascos@gmail.com>
2023-04-26 23:36:53 +0100

    update

--- Week 4/automate_email_sending.py
+++ Week 4/automate_email_sending.py
@@ -14,3 +14,24 @@ Here are the steps you can take to automate this process:
 
     You can also set up a log file to keep track of the emails that have been sent and any errors that may have occurred during the email sending process. '''
 
+import smtplib
+from email.mime.text import MIMEText
+import os
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtp_server.login(sender, password)
+    smtp_server.sendmail(sender, recipients, msg.as_string())
+    smtp_server.quit()
+
+subject = "Email Subject"
+body = "This is the body of the text message"
+sender = "sender@gmail.com"
+recipients = ["recipient1@gmail.com", "recipient2@gmail.com"]
+password = "password"
+
+send_email(subject, body, sender, recipients, password)
