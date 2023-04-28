563528b2effee400ae64ca75872db516a2273bc8
ncx01 <nicholascos@gmail.com>
2023-04-28 22:57:42 +0100

    Finish week 3 assignments

--- Week 4/automate_email_sending.py
+++ Week 4/automate_email_sending.py
@@ -14,24 +14,67 @@ Here are the steps you can take to automate this process:
 
     You can also set up a log file to keep track of the emails that have been sent and any errors that may have occurred during the email sending process. '''
 
-import smtplib
+import smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+
+# MIMEText() objects will contain the HTML and plain-text versions of the message
 from email.mime.text import MIMEText
+
+# MIMEMultipart("alternative") instance combines these into a single message with two alternative rendering options
+from email.mime.multipart import MIMEMultipart
 import os
 
-def send_email(subject, body, sender, recipients, password):
-    msg = MIMEText(body)
+def send_email(subject, body, filename, sender, recipients, password):
+
+    msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = ', '.join(recipients)
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.login(sender, password)
-    smtp_server.sendmail(sender, recipients, msg.as_string())
-    smtp_server.quit()
 
-subject = "Email Subject"
-body = "This is the body of the text message"
-sender = "sender@gmail.com"
+    # Add body to email
+    msg.attach(MIMEText(body, "plain"))
+
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
+
+    # Add attachment to message and convert message to string
+    msg.attach(part)
+    text = msg.as_string()
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+
+subject = "Multipart Test"
+
+body = "This is an email with attachment sent from Python"
+
+filename = "document.pdf"
+
+# Your email address
+sender = "sender@gmail.com" 
+
+# Your email password, should use input as it is not safe to store inside code, can import getpass module and
+# use getpass() for blind input
+password = input("Type your password and press enter: ")
+
 recipients = ["recipient1@gmail.com", "recipient2@gmail.com"]
-password = "password"
 
-send_email(subject, body, sender, recipients, password)
+send_email(subject, body, filename, sender, recipients, password)
